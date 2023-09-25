a0e81b56b6467361b61d633eebdeb8b4ba80fffb
mridul0702 <guptamridul0207@gmail.com>
2023-09-25 22:49:11 +0530

    error resolved

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 exports.connect = ()=>{
-    console.log(process.env.MONGODB_URL)
+    
     mongoose.connect(process.env.MONGODB_URL,{
         useNewUrlParser:true,
         useUnifiedTopology:true
